edf98fc6084898090c1f1608d61e045b9832f2fa
Brian Kiernan <brian@briankiernan.com>
2020-09-22 20:26:50 -0400

    Update menu-items withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+function HatsPage() {
+  return <div>HATS PAGE</div>;
+}
 
 export default function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-export default function Homepage() {
+export default function HomePage() {
   return (
     <div className="homepage">
       <Directory />
