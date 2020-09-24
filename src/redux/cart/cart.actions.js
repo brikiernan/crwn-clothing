d3fbd7d7cda4c864ed512a5cfaba7b5aea48b442
Brian Kiernan <brian@briankiernan.com>
2020-09-24 10:39:37 -0400

    Update cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,7 +1,14 @@
-import { TOGGLE_CART_HIDDEN } from './cart.types';
+import CartActionTypes from './cart.types';
 
 export function toggleCartHidden() {
   return {
-    type: TOGGLE_CART_HIDDEN
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
+  };
+}
+
+export function addItem(item) {
+  return {
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
   };
 }
