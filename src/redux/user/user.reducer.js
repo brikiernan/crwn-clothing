03b541a2f079cff4a6476669c4ccb59fde242ef6
Brian Kiernan <brian@briankiernan.com>
2020-09-24 08:16:47 -0400

    Redirect to home if user signed in, add redux const(s)

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,8 +1,10 @@
+import { SET_CURRENT_USER } from './user.types';
+
 const INITIAL_STATE = { currentUser: null };
 
 export default function userReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
